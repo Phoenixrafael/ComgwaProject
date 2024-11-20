de01975fe546da520cdab51cb34afd3502b04bc2
kdy40929 <doyun410@gmail.com>
2024-11-20 11:21:42 +0900

    cutscenes 만들기 시작

---
+++ cutscenes.py
@@ -0,0 +1,10 @@
+import pygame, pygame.event, pygame.locals
+import comgwa
+
+mountain = pygame.image.load("asset/sprite/background/bigThumb.png")
+bus = pygame.image.load("asset/sprite/background/bus.png")
+camp1 = pygame.image.load("asset/sprite/background/camp1.png")
+camp2 = pygame.image.load("asset/sprite/background/camp2.png")
+camp3 = pygame.image.load("asset/sprite/background/camp3.png")
+lawcourt = pygame.image.load("asset/sprite/background/lawCourt.png")
+town = pygame.image.load("asset/sprite/background/town.png")
